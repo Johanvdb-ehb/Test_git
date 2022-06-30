e011988a4a73b505eedabd7799df4c1d3a83d63e
Johan van den Broek <johan.van.den.broek@ehb.be>
2022-06-30 11:28:04 +0200

    Initial Commit

---
+++ Test_git/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test_git
+//
+//  Created by Johan van den Broek on 30/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
